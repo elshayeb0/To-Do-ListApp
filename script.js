f6589d8c60378e6d9895767235a661ce227d42bc
elshayeb0 <ziad.elshayeb62@gmail.com>
2025-07-29 16:44:12 +0300

    Initial Application Structure

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    //Initilaized my essential HTML Elements
+const taskForm = document.getElementById('input-task-form');
+const inputField = document.getElementById('task-input');
+const listView = document.getElementById('task-list-view');
+
+let tasks =[];   // User's tasks will be regiserted here for any task manipulation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
